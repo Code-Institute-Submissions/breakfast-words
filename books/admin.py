1493748d5051ea66d317c4b313e8190aad253d9c
james-lancaster <jameslancaster@outlook.com>
2021-11-08 23:59:48 +0000

    add books view

--- books/admin.py
+++ books/admin.py
@@ -2,3 +2,28 @@ from django.contrib import admin
 from .models import Book, Category
 admin.site.register(Book)
 admin.site.register(Category)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author_surname',
+        'author_firstname',
+        'price',
+        'publisher',
+        'date',
+        'isbn',
+        'book_format',
+        'pages',
+        'sku',
+        'description'
+        'category'
+        'image_url'
+    )
+    ordering = ('sku',)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
--- books/models.py
+++ books/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
+    class Meta:
+    verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -16,7 +19,6 @@ class Book(models.Model):
     author_firstname = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     publisher = models.CharField(max_length=254, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     date = models.DateField()
     isbn = models.IntegerField()
     book_format = models.CharField(max_length=254, null=True, blank=True)
