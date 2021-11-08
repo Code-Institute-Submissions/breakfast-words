6087317538eda4363c13509dd1fb3ec43ffaf2d7
james-lancaster <jameslancaster@outlook.com>
2021-11-08 23:41:31 +0000

    set up books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Book(models.Model):
+    title = models.CharField(max_length=254, null=True, blank=True)
+    author_surname = models.CharField(max_length=254, null=True, blank=True)
+    author_firstname = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    date = models.DateField()
+    isbn = models.IntegerField()
+    book_format = models.CharField(max_length=254, null=True, blank=True)
+    pages = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
