b413f554379d87206a685f8a5568d309727e1897
james-lancaster <jameslancaster@outlook.com>
2021-11-09 21:35:22 +0000

    adding categories to books

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Book
+from .models import Book, Category
 
 
 def all_books(request):
@@ -10,13 +10,21 @@ def all_books(request):
     books = Book.objects.all()
     query = None
 
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            books = books.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "What book are you looking for?")
                 return redirect(reverse('books'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             books = books.filter(queries)
 
