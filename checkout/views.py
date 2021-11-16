c4345788a9b5338f862baf583b7e563b2dea0255
james-lancaster <jameslancaster@outlook.com>
2021-11-16 23:34:59 +0000

    modification to stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('books'))
+
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
@@ -17,5 +35,5 @@ def checkout(request):
         'stripe_public_key': 'pk_live_51JjPYqBBi58M3Z5kI1r10PKJeqNMPkNYvuEmP09TK7XoqOus62RBUEgo4sX4SKTvOqCZEcbGvUq1y9z6dx07expM00f5GdS2Uz',
         'client_secret': 'test client secret',
     }
-    
+
     return render(request, template, context)
