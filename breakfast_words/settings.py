b43316de25e954e103f2c4b47fd1b38e7baab827
james-lancaster <jameslancaster@outlook.com>
2021-11-23 00:29:19 +0000

    adjustment to settings

--- breakfast_words/settings.py
+++ breakfast_words/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-*ie0#272*1@trx6(4+)d$r@)@i@=lvefc%0u^)4f!896n+=5a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
