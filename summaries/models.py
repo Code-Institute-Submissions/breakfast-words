b5cc835137e009d51551eeac6e77b86ace886e6c
james-lancaster <jameslancaster@outlook.com>
2021-11-26 11:24:25 +0000

    changes to summaries

--- summaries/models.py
+++ summaries/models.py
@@ -4,14 +4,12 @@ from books.models import Book
 
 
 class Summary(models.Model):
-    title = models.ForeignKey(default=(), max_length=254, null=False, blank=False)
-    author_surname = models.ForeignKey(max_length=254, null=False, blank=False)
-    author_firstname = models.ForeignKey(max_length=254, null=False, blank=False)
+    title = models.ForeignKey("Books", max_length=254, null=False, blank=False, default="")
+    author_surname = models.ForeignKey("Books", max_length=254, null=False, blank=False, default="")
+    author_firstname = models.ForeignKey("Books", max_length=254, null=False, blank=False, default="")
     date_summary = models.DateField()
     content_summary = models.TextField()
-    category = models.ForeignKey('Category', null=True, blank=True,
-                                 on_delete=models.SET_NULL)
-    image = models.ForeignKey(null=True, blank=True)
+    image = models.ForeignKey("Books", max_length=254, null=False, blank=False, default="")
 
     def __str__(self):
         return self.name
