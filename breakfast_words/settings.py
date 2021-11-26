8e82345d1f7497559840eb742590735377aedbad
james-lancaster <jameslancaster@outlook.com>
2021-11-26 09:37:15 +0000

    correction of secret key error

--- breakfast_words/settings.py
+++ breakfast_words/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECURITY WARNING: don't run with debug turned on in production!
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['breakfast-words.herokuapp.com', 'localhost']
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'basket',
     'checkout',
     'profiles',
+    'summaries',
 
     # Other
     'crispy_forms',
@@ -176,6 +177,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 #     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
+
+# Static and media files
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+
+# Override static and media URLs in production
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
--- summaries/models.py
+++ summaries/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+from books.models import Book
+
+
 class Summary(models.Model):
-    title = books.models.ForeignKey(default=(), max_length=254, null=False, blank=False)
-    author_surname = books.models.ForeignKey(max_length=254, null=False, blank=False)
-    author_firstname = books.models.ForeignKey(max_length=254, null=False, blank=False)
+    title = models.ForeignKey(default=(), max_length=254, null=False, blank=False)
+    author_surname = models.ForeignKey(max_length=254, null=False, blank=False)
+    author_firstname = models.ForeignKey(max_length=254, null=False, blank=False)
     date_summary = models.DateField()
     content_summary = models.TextField()
-    category = books.models.ForeignKey('Category', null=True, blank=True,
+    category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     image = models.ForeignKey(null=True, blank=True)
 
